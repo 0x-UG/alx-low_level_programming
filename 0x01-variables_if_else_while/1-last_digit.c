a34d4db9efc6b4a52d8801415d21c870a2e73aba
Ugoo1290 <okekecee12@gmail.com>
2023-07-14 06:16:02 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		printf("is %d and is greater than 5\n", n % 10);
 	}
-	else if ((n < 6) && != 0)
+	else if ((n < 6) && (n % 10) != 0)
 	{
 		printf("is %d and is less than 6 and not 0\n", n % 10);
 	}
