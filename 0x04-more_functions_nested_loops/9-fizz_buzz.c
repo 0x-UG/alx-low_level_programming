4af101ec8a75eec4d914b8a919123218a237587b
Ugoo1290 <okekecee12@gmail.com>
2023-07-20 18:55:28 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,15 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-		printf("%d ", i);
+	{
 	if (i % 3 == 0)
 		printf("Fizz");
 	else if (i % 5 == 0)
 		printf("Buzz");
 	else if (i % 3 == 0 && i && 5 == 0)
 		printf("FizzBuzz");
+	}
+	printf("%d ", i);
 
 	return (0);
 }
