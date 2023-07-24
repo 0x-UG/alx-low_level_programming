17f083095eaa4c6610f2ee70afbebf064a085b17
Ugoo1290 <okekecee12@gmail.com>
2023-07-24 13:50:00 -0700

    printrev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,12 @@
 
 void print_rev(char *s)
 {
-	int len = 0;
 	int i;
+	int c = 0;
 
-	while (s[len] != '\n')
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; s[i] != '\0'; i++)
+		c++;
+	for (i = c - 1; s[i] >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
